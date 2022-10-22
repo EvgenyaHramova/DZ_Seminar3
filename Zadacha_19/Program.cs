7161c3cfd8b1788e44796b1307e5c43a0825407a
EvgenyaHramova <evgenya.hramova@yandex.ru>
2022-10-22 19:42:44 +0400

    Izm folder Zadacha19

---
+++ Zadacha_19/Program.cs
@@ -0,0 +1,30 @@
+﻿// Напишите программу, которая принимает на вход пятизначное число и проверяет, является ли оно палиндромом.
+// 14212 -> нет
+// 23432 -> да
+// 12821 -> да
+
+string Palindrome(int num)
+{
+    string text = "";
+    int a = num / 1000;
+    int b = num % 100;
+    int a1 = a / 10;
+    int a2 = a % 10;
+    int a4 = b / 10;
+    int a5 = b % 10;
+    if (a1 == a5 && a2 == a4 && num > 0)
+    {
+        text = "Число является палиндромом.";
+    }
+    else
+    {
+        text = "Число не является палиндромом.";
+    }
+    return text;
+}
+
+Console.WriteLine("Введите пятизначное число: ");
+int fiveNum = Convert.ToInt32(Console.ReadLine());
+
+string result = Palindrome(fiveNum);
+Console.WriteLine(result);
