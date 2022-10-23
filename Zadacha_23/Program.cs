a678ca71d1fcbe489100d0264caf25687d8381b8
EvgenyaHramova <evgenya.hramova@yandex.ru>
2022-10-23 19:43:21 +0400

    New folder Zadacha23

---
+++ Zadacha_23/Program.cs
@@ -0,0 +1,23 @@
+﻿// Напишите программу, которая принимает на вход число (N)
+// и выдаёт таблицу кубов чисел от 1 до N.
+// 3 -> 1, 4, 9. 
+// 5 -> 1, 8, 27, 64, 125
+
+
+int CubeNum(int x)
+{
+    int i = 1;
+    while (i<=x)
+    {
+        Console.WriteLine(Convert.ToInt32(Math.Pow(i, 3)));
+        i++;
+    }
+    return x;
+}
+
+Console.WriteLine("Введите число: ");
+int x = Convert.ToInt32(Console.ReadLine());
+
+int result = CubeNum(x);
+Console.WriteLine(result);
+
