4d10d8906f6e03ae7fbef11387c485465a19538c
EvgenyaHramova <evgenya.hramova@yandex.ru>
2022-10-24 20:07:36 +0400

    New folder Zadacha23_1

---
+++ Zadacha_23_1/Program.cs
@@ -0,0 +1,20 @@
+﻿// Напишите программу, которая принимает на вход число (N)
+// и выдаёт таблицу кубов чисел от 1 до N.
+// 3 -> 1, 8, 27. 
+// 5 -> 1, 8, 27, 64, 125
+
+
+void PrintCube(int x)
+{
+    int i = 1;
+    while (i<=x)
+    {
+        Console.WriteLine(Convert.ToInt32(Math.Pow(i, 3)));// возведение в степень и перевод
+        i++;
+    }
+}
+
+Console.Write("Введите число: ");
+int x = Convert.ToInt32(Console.ReadLine());
+
+PrintCube(x);
